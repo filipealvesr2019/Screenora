56ebac7c86e7bb92172bcd5a49ffb45372e04320
filipealvesrds2019 <filipealvesrds2019@gmail.com>
2026-05-11 12:16:45 -0300

    ad

--- extension Screenora/background.js
+++ extension Screenora/background.js
@@ -1,23 +1,21 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({
+    url: `https://page-viewer-sepia.vercel.app/?url=${encodeURIComponent(tab.url)}`
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "openInScreenora") {
+    chrome.tabs.create({
+      url: `https://page-viewer-sepia.vercel.app/?url=${encodeURIComponent(info.pageUrl)}`
+    });
+  }
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "openInScreenora",
     title: "Abrir no Screenora",
     contexts: ["page"]
   });
-});
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "openInScreenora" && tab.url) {
-    const screenoraUrl = `http://localhost:3001/?url=${encodeURIComponent(tab.url)}`;
-    chrome.tabs.create({ url: screenoraUrl });
-  }
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  if (tab.url && !tab.url.startsWith('chrome://')) {
-    const screenoraUrl = `http://localhost:3001/?url=${encodeURIComponent(tab.url)}`;
-    chrome.tabs.create({ url: screenoraUrl });
-  } else {
-    chrome.tabs.create({ url: 'http://localhost:3001' });
-  }
 });
